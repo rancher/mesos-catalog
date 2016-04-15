d39af388fece25a8ab0c1473025ba5b6a5a780d1
James Oliver <joliver@rancher.com>
2016-04-15 03:07:53 -0700

    Organize

--- images/mesos/build.py
+++ images/mesos/build.py
@@ -4,11 +4,13 @@ from subprocess import call
 from os import remove
 
 user='llparse'
-#versions=['0.24.1-centos-7', '0.24.1-ubuntu-14.04']
-versions=['0.24.1-centos-7']
+versions=['0.24.1-centos-7', '0.24.1-ubuntu-14.04']
 types=['master', 'slave']
 
-env = Environment(loader=FileSystemLoader('.'))
+env = Environment(
+  loader=FileSystemLoader('./templates'),
+  trim_blocks=True)
+
 for version in versions:
   for t in types:
     image='{0}/mesos-{1}:{2}'.format(user, t, version)
@@ -17,15 +19,20 @@ for version in versions:
 
     with open(dockerfile, 'w') as f:
       template = env.get_template('Dockerfile.j2')
-      f.write(template.render(version=version, type=t))
+      f.write(template.render(
+        version=version, 
+        type=t,
+        entrypoint=entrypoint))
     
     with open(entrypoint, 'w') as f:
       template = env.get_template('entrypoint.sh.j2')
-      f.write(template.render(version=version, type=t))
+      f.write(template.render(
+        version=version, 
+        type=t, 
+        entrypoint=entrypoint))
     
     call(['chmod', '+x', entrypoint])
     call(['docker', 'build', '-f', dockerfile, '-t', image, '.'])
     call(['docker', 'push', image])
-
     remove(dockerfile)
     remove(entrypoint)
